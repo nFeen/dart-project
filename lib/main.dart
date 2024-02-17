35dfa7655e4744cc64c5b639e6ef353c4e9e906e
nfeen <dima.kolin.00@mail.ru>
2024-02-17 14:48:42 +0300

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Дренин Кирилл Александрович БСБО-12-22 22Б0650',
+              'Дренин Кирилл Александрович БСБО-12-22 22Б0650 v2',
             ),
           ],
         ),
