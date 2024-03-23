aa7187acf7aaa52ecc0a44780ad97d5083adc84f
nfeen <dima.kolin.00@mail.ru>
2024-03-23 15:59:55 +0300

    three different lists

--- lib/main.dart
+++ lib/main.dart
@@ -31,58 +31,94 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  final items = List.generate(5, (index) => 'Item ${index + 1}');
+  final items1 = List.generate(5, (index) => 'Item ${index + 1}');
+  final items2 = List.generate(5, (index) => 'Item ${index + 1}');
+
+  void addItem() {
+    setState(() {
+      if (_selectedIndex == 0) {
+        items.add('Item ${items.length + 1}');
+      }
+      else{
+        items1.add('Item ${items1.length + 1}');
+      }
+      if (_selectedIndex == 2) {
+        items2.add('Item ${items2.length + 1}');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> _widgetOptions = <Widget>[
-      const Center(
-        child: Text(
-          'Welcome to my program!',
-          style: TextStyle(fontSize: 24.0),
-        ),
-      ),
+    List<Widget> widgetOptions = <Widget>[
       Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter your name',
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Submit'),
-            ),
-          ],
-        ),
-      ),
-      Center(
-        child: GestureDetector(
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.blue,
-            child: const Center(
-              child: Text(
-                'Go to the next page!',
-                style: TextStyle(color: Colors.white, fontSize: 18.0),
-              ),
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: items.asMap().entries.map((entry) {
+              int index = entry.key;
+              String item = entry.value;
+              return SizedBox(
+                width: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Text(item),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        setState(() {
+                          items.removeAt(index);
+                        });
+                      },
+                    ),
+                  ],
+                )
+              );
+            }).toList(),
           ),
+        )
+      ),
+      Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: items1
+              .map((item) => Center(
+                  child: GestureDetector(
+                    key: ValueKey(item),
+                    onTap: () => setState(() => items1.remove(item)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Text(item),
+                    )
+                  )
+                )
+              )
+              .toList(),
         ),
       ),
       Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Click me!'),
-        ),
-      ),
-      const Center(
-        child: Icon(
-          Icons.favorite,
-          size: 100.0,
-          color: Colors.red,
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemBuilder: (_, position) {
+            final item = items2[position];
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  items2.removeAt(position);
+                });
+              },
+              child: Text(
+                item,
+                key: ValueKey(item),
+              ),
+            );
+          },
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: items2.length,
         ),
       ),
     ];
@@ -91,28 +127,32 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Program'),
       ),
-      body: _widgetOptions.elementAt(_selectedIndex),
+      body: widgetOptions.elementAt(_selectedIndex),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: addItem,
+            tooltip: 'Add',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon(Icons.add_circle_outline_rounded),
+            label: 'Column',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
+            icon: Icon(Icons.add_circle_outline_rounded),
+            label: 'ListView',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.navigate_next),
-            label: 'Next Page',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.touch_app),
-            label: 'Click Me',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorite',
+            icon: Icon(Icons.add_circle_outline_rounded),
+            label: 'Separated',
           ),
         ],
         currentIndex: _selectedIndex,
